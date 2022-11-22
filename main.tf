626917c889033c224973289c64308dfa6e5e4b82
Andrey.Torshin <dchorch@gmail.com>
2022-11-22 04:07:34 -0800

    v4

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
   required_version = ">= 0.13"
 }
 
-#provider "yandex" {
-# token     = var.token
-# cloud_id  = var.cloud_id
-# folder_id = var.folder_id
-# zone      = "ru-central1-a"
-#}
+provider "yandex" {
+ token     = var.token
+ cloud_id  = var.cloud_id
+ folder_id = var.folder_id
+ zone      = "ru-central1-a"
+}
 
 resource "yandex_compute_instance" "vm" {
     count = 3
