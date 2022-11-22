969d9cfd654b4750ac4e88504160a42fd81b3f5d
Andrey.Torshin <dchorch@gmail.com>
2022-11-22 04:12:06 -0800

    v6

--- main.tf
+++ main.tf
@@ -1,79 +1,48 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+    yandex = {
+      source = "terraform-registry.storage.yandexcloud.net/yandex-cloud/yandex"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 0.13"
+}
 
-  cloud {
-    organization = "REPLACE_ME"
+provider "yandex" {
+ token     = var.token
+ cloud_id  = var.cloud_id
+ folder_id = var.folder_id
+ zone      = "ru-central1-a"
+}
 
-    workspaces {
-      name = "gh-actions-demo"
+resource "yandex_compute_instance" "vm" {
+    count = 3
+    name = "vm${count.index}"
+    resources {
+        cores  = 2
+        memory = 6
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id = "fd80jfslq61mssea4ejn"
+        }
+    }
+
+    network_interface {
+        subnet_id = "e9bhp1sm4upq328p2jm4"
+        nat       = true
+    }
+
+    metadata = {
+        user-data = file(var.new_user)
     }
   }
+
+output "instance_output" {
+  value = [
+    for instance in yandex_compute_instance.vm[*] :
+    join(": ", [instance.name, instance.hostname, instance.network_interface.0.ip_address, instance.network_interface.0.nat_ip_address])
+  ]
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
