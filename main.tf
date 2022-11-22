4f3c69aeffafbe76a6d5d52f10c94f0fbef1f3cc
Andrey.Torshin <dchorch@gmail.com>
2022-11-22 04:34:22 -0800

    v16

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
- token     = "${{ secrets.YA_TOKEN }}"
+ token     = var.token
  cloud_id  = var.cloud_id
  folder_id = var.folder_id
  zone      = "ru-central1-a"
